bbc72b1e6ff4c4c549f5a5d49f4f8f4ff55280c1
tory ventura <tory.16@outlook.com>
2019-01-25 14:48:37 -0400

    first commit

---
+++ FacturacionSIM/App_Start/UnityConfig.cs
@@ -0,0 +1,24 @@
+using FacturacionSIM.Clases;
+using System.Web.Mvc;
+using Unity;
+using Unity.Mvc5;
+
+namespace FacturacionSIM
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+
+			// register all your components with the container here
+			// it is NOT necessary to register your controllers
+
+			// e.g. container.RegisterType<ITestService, TestService>();
+			container.RegisterType<ICompomentes, Compomentes>();
+			
+			DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+			
+        }
+    }
+}
